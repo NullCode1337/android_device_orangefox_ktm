e950a04e7c9d291c99a8c25e31686b1a94e50f18
Darth9 <reyprimerjefe@gmail.com>
2025-11-09 20:00:27 +0000

    Dispense with prebuilt se_omapi and build from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2024 The OrangeFox Recovery Project
+# Copyright (C) 2024-2025 The OrangeFox Recovery Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
@@ -180,3 +180,7 @@ TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# se_omapi
+TW_INCLUDE_OMAPI := true
+#
